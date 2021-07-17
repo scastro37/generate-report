81695a1f9f675c8ad7a434a0e7dfd6e45632a605
Sebastian Castro <80695969+scastro37@users.noreply.github.com>
2021-07-16 20:12:33 -0500

    Fix: solved problem with undefined data

--- index.js
+++ index.js
@@ -4,18 +4,19 @@ const logger = require('@condor-labs/logger');
 const writeCsvFile = async (filePath, data) => {
   try {
     const headers = new Map();
-    for (const element of data) {
-      for (const key in element) {
-        if (key !== 'action') {
+    for (const element of data.filter(Boolean)) {
+      for (const key of Object.keys(element)) {
+        if (key !== 'action' && key !== '$init') {
           headers.set(key, { id: key, title: key });
         }
       }
     }
+
     const csvWriter = createCsvWriter({
       path: filePath,
       header: [...headers.values(), { id: 'action', title: 'action' }],
     });
-    await csvWriter.writeRecords(data);
+    await csvWriter.writeRecords(data.filter(Boolean));
   } catch (error) {
     logger.log(error);
   }
